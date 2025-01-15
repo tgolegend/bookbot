0fcfc8168dbe8052de0631b3a0f1eb0dd71d5140
tgolegend <thegamesoflegend@gmail.com>
2025-01-15 12:30:11 +0100

    Count words

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def get_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
     text = get_text("books/frankenstein.txt")
-    print(text)
+    count = count_words(text)
+    print(f"{count} words found in the text")
 
 main()
